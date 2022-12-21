b6b5190e7f4174a79ee53a167ddc8db272c07456
illumination-k <illumination.k.27@gmail.com>
2022-12-21 17:01:46 +0900

    fix importGenome because of ent entity def

--- server/cmd/importgenome/importgenome.go
+++ server/cmd/importgenome/importgenome.go
@@ -9,7 +9,6 @@ import (
 	"genomedb/seq"
 	"genomedb/seqio"
 	"os"
-	"strconv"
 	"strings"
 )
 
@@ -45,7 +44,6 @@ func Run(genomeName string, genomeFasta string, genomeGff string, databaseUri st
 	}
 
 	// ## create genome
-
 	if err := client.Genome.Create().SetID(genomeName).SetCodonTable(codonCode).OnConflict().UpdateNewValues().Exec(ctx); err != nil {
 		return err
 	}
@@ -55,18 +53,21 @@ func Run(genomeName string, genomeFasta string, genomeGff string, databaseUri st
 
 	genes := map[string]struct{}{}
 	transcriptDtos := []*ent.TranscriptCreate{}
-	transcriptDataCreates := TranscriptDataCreates{}
 
 	for transcriptId, rec := range transcriptId2recs {
 		scaffoldSeq, ok := seqname2seq[rec.SeqName]
 
-		genes[rec.GeneId] = struct{}{}
+		genes[rec.LocusId] = struct{}{}
 
 		if !ok {
 			return fmt.Errorf("seqname=%v does not exist in %v\n rec: %v\n", rec.SeqName, genomeFasta, rec)
 		}
 
-		transcriptSeq := rec.ToTranscriptSeq(scaffoldSeq, codonTable)
+		cds := rec.CdsSequence(scaffoldSeq)
+		protein, _ := codonTable.Transrate(cds)
+		exon := rec.ExonSequence(scaffoldSeq)
+		genomic := rec.GenomicSequence(scaffoldSeq)
+
 		transcriptDtos = append(
 			transcriptDtos,
 			client.Transcript.
@@ -74,35 +75,38 @@ func Run(genomeName string, genomeFasta string, genomeGff string, databaseUri st
 				SetID(transcriptId).
 				SetStrand(rec.Strand).
 				SetType(rec.Type).
-				SetGenomeSeq(transcriptSeq.Genome).
-				SetTranscriptSeq(transcriptSeq.Mrna).
-				SetCdsSeq(transcriptSeq.Cds).
-				SetProteinSeq(transcriptSeq.Protein).
-				SetGeneID(rec.GeneId),
+				SetSeqname(rec.SeqName).
+				SetStart(rec.Start).
+				SetEnd(rec.End).
+				SetGenomicSequence(genomic).
+				SetExon(rec.ExonRecords).
+				SetExonSequence(exon).
+				SetCds(rec.CdsRecords).
+				SetCdsSequence(cds).
+				SetProteinSequence(protein).
+				SetFivePrimeUtr(rec.FivePrimeUtrRecords).
+				SetThreePrimeUtr(rec.ThreePrimeUtrRecords).
+				SetLocusID(rec.LocusId),
 		)
-
-		for _, gffrec := range rec.Records {
-			transcriptDataCreates.PushGffRecord(client, transcriptId, gffrec)
-		}
 	}
 
-	geneDtos := []*ent.GeneCreate{}
+	geneDtos := []*ent.LocusCreate{}
 	for geneId := range genes {
-		geneDtos = append(geneDtos, client.Gene.Create().SetID(geneId).SetGenomeID(genomeName))
+		geneDtos = append(geneDtos, client.Locus.Create().SetID(geneId).SetGenomeID(genomeName))
 	}
 
 	stepNum := 100
 
-	fmt.Printf("Upsert %d Genes ...\n", len(geneDtos))
+	fmt.Printf("Upsert %d Locuses ...\n", len(geneDtos))
 	for i := 0; i < len(geneDtos); i += stepNum {
-		var dtos []*ent.GeneCreate
+		var dtos []*ent.LocusCreate
 		if i+stepNum > len(geneDtos) {
 			dtos = geneDtos[i:]
 		} else {
 			dtos = geneDtos[i : i+stepNum]
 		}
 
-		if err := client.Gene.CreateBulk(dtos...).OnConflict().UpdateNewValues().Exec(ctx); err != nil {
+		if err := client.Locus.CreateBulk(dtos...).OnConflict().UpdateNewValues().Exec(ctx); err != nil {
 			return err
 		}
 	}
@@ -116,121 +120,6 @@ func Run(genomeName string, genomeFasta string, genomeGff string, databaseUri st
 			dtos = transcriptDtos[i : i+stepNum]
 		}
 		if err := client.Transcript.CreateBulk(dtos...).OnConflict().UpdateNewValues().Exec(ctx); err != nil {
-			return err
-		}
-	}
-
-	transcriptDataCreates.BulkCreate(client, ctx, stepNum)
-
-	return nil
-}
-
-type TranscriptDataCreates struct {
-	Cds           []*ent.CdsCreate
-	Exon          []*ent.ExonCreate
-	FivePrimeUtr  []*ent.FivePrimeUtrCreate
-	ThreePrimeUtr []*ent.ThreePrimeUtrCreate
-}
-
-func (c *TranscriptDataCreates) PushGffRecord(client *ent.Client, transcriptId string, rec gffio.GffRecord) {
-	seqname := rec.Seqname
-	start := rec.Start
-	end := rec.End
-	strand := rec.Strand
-
-	if rec.IsCds() {
-		_frame, _ := strconv.Atoi(rec.Phase)
-		var frame int8 = int8(_frame)
-		c.Cds = append(c.Cds,
-			client.Cds.Create().
-				SetTranscriptID(transcriptId).
-				SetSeqname(seqname).
-				SetStart(start).
-				SetEnd(end).
-				SetPhase(frame).
-				SetStrand(strand),
-		)
-	} else if rec.IsExon() {
-		c.Exon = append(
-			c.Exon,
-			client.Exon.Create().
-				SetTranscriptID(transcriptId).
-				SetSeqname(seqname).
-				SetStart(start).
-				SetEnd(end).
-				SetStrand(strand),
-		)
-	} else if rec.IsFivePrimeUtr() {
-		c.FivePrimeUtr = append(
-			c.FivePrimeUtr,
-			client.FivePrimeUtr.Create().
-				SetTranscriptID(transcriptId).
-				SetSeqname(seqname).
-				SetStart(start).
-				SetEnd(end).
-				SetStrand(strand),
-		)
-	} else if rec.IsThreePrimeUtr() {
-		c.ThreePrimeUtr = append(c.ThreePrimeUtr,
-			client.ThreePrimeUtr.Create().
-				SetTranscriptID(transcriptId).
-				SetSeqname(seqname).
-				SetStart(start).
-				SetEnd(end).
-				SetStrand(strand),
-		)
-	}
-}
-
-func (c TranscriptDataCreates) BulkCreate(client *ent.Client, ctx context.Context, stepNum int) error {
-	fmt.Printf("Upsert %d Exons ...\n", len(c.Exon))
-	for i := 0; i < len(c.Exon); i += stepNum {
-		var dtos []*ent.ExonCreate
-		if i+stepNum > len(c.Exon) {
-			dtos = c.Exon[i:]
-		} else {
-			dtos = c.Exon[i : i+stepNum]
-		}
-		if err := client.Exon.CreateBulk(dtos...).OnConflict().UpdateNewValues().Exec(ctx); err != nil {
-			return err
-		}
-	}
-
-	fmt.Printf("Upsert %d Cds ...\n", len(c.Cds))
-	for i := 0; i < len(c.Cds); i += stepNum {
-		var dtos []*ent.CdsCreate
-		if i+stepNum > len(c.Cds) {
-			dtos = c.Cds[i:]
-		} else {
-			dtos = c.Cds[i : i+stepNum]
-		}
-		if err := client.Cds.CreateBulk(dtos...).OnConflict().UpdateNewValues().Exec(ctx); err != nil {
-			return err
-		}
-	}
-
-	fmt.Printf("Upsert %d FivePrimeUtrs ...\n", len(c.FivePrimeUtr))
-	for i := 0; i < len(c.FivePrimeUtr); i += stepNum {
-		var dtos []*ent.FivePrimeUtrCreate
-		if i+stepNum > len(c.FivePrimeUtr) {
-			dtos = c.FivePrimeUtr[i:]
-		} else {
-			dtos = c.FivePrimeUtr[i : i+stepNum]
-		}
-		if err := client.FivePrimeUtr.CreateBulk(dtos...).OnConflict().UpdateNewValues().Exec(ctx); err != nil {
-			return err
-		}
-	}
-
-	fmt.Printf("Upsert %d ThreePrimeUtrs ...\n", len(c.ThreePrimeUtr))
-	for i := 0; i < len(c.ThreePrimeUtr); i += stepNum {
-		var dtos []*ent.ThreePrimeUtrCreate
-		if i+stepNum > len(c.ThreePrimeUtr) {
-			dtos = c.ThreePrimeUtr[i:]
-		} else {
-			dtos = c.ThreePrimeUtr[i : i+stepNum]
-		}
-		if err := client.ThreePrimeUtr.CreateBulk(dtos...).OnConflict().UpdateNewValues().Exec(ctx); err != nil {
 			return err
 		}
 	}
@@ -274,56 +163,83 @@ func ReadGenomeFasta(fastaPath string) (map[string]string, error) {
 
 // reading a gff file
 type TranscriptRecords struct {
-	GeneId  string
-	Type    string
-	SeqName string
-	Strand  string
-	Records []gffio.GffRecord
+	LocusId              string
+	Type                 string
+	SeqName              string
+	Strand               string
+	Start                int32
+	End                  int32
+	CdsRecords           []gffio.GffRecord
+	ExonRecords          []gffio.GffRecord
+	FivePrimeUtrRecords  []gffio.GffRecord
+	ThreePrimeUtrRecords []gffio.GffRecord
 }
 
-type TranscriptSeq struct {
-	Genome  string
-	Mrna    string
-	Cds     string
-	Protein string
+func NewTranscriptRecords(record gffio.GffRecord) (TranscriptRecords, error) {
+	transcriptRecord := new(TranscriptRecords)
+
+	if !record.IsGeneChild() {
+		return *transcriptRecord, fmt.Errorf("New transcript record should be created from gene child")
+	}
+
+	geneId, found := record.Attributes.Get("Parent")
+
+	if !found {
+		return *transcriptRecord, fmt.Errorf("")
+	}
+
+	transcriptRecord.LocusId = geneId
+	transcriptRecord.Type = record.Type
+	transcriptRecord.SeqName = record.Seqname
+	transcriptRecord.Start = record.Start
+	transcriptRecord.End = record.End
+	transcriptRecord.Strand = record.Strand
+
+	return *transcriptRecord, nil
 }
 
-func (r TranscriptRecords) ToTranscriptSeq(scaffoldSeq string, codonTable seq.CodonTable) TranscriptSeq {
-	var genome string
-	var mrna strings.Builder
+func (r *TranscriptRecords) PushGffRecord(record gffio.GffRecord) error {
+	if record.IsCds() {
+		r.CdsRecords = append(r.CdsRecords, record)
+	} else if record.IsExon() {
+		r.ExonRecords = append(r.ExonRecords, record)
+	} else if record.IsFivePrimeUtr() {
+		r.FivePrimeUtrRecords = append(r.FivePrimeUtrRecords, record)
+	} else if record.IsThreePrimeUtr() {
+		r.ThreePrimeUtrRecords = append(r.ThreePrimeUtrRecords, record)
+	} else {
+	}
+
+	return nil
+}
+
+func (r *TranscriptRecords) CdsSequence(scaffoldSeq string) string {
 	var cds strings.Builder
 
-	for _, rec := range r.Records {
-		seq := scaffoldSeq[rec.Start-1 : rec.End]
-
-		if rec.IsGeneChild() {
-			genome = seq
-		} else if rec.IsExon() {
-			mrna.WriteString(seq)
-		} else if rec.IsCds() {
-			cds.WriteString(seq)
-		} else {
-			// unknown feature
-		}
+	for _, r := range r.CdsRecords {
+		cds.WriteString(r.ExtractSequence(scaffoldSeq))
 	}
 
-	var Cds string
-	var Mrna string
-	if r.Strand == "+" {
-		Cds = cds.String()
-		Mrna = mrna.String()
-	} else {
-		Cds = seq.ReverseComplement(cds.String())
-		Mrna = seq.ReverseComplement(cds.String())
+	return cds.String()
+}
+
+func (r *TranscriptRecords) ExonSequence(scaffoldSeq string) string {
+	var exon strings.Builder
+	for _, r := range r.ExonRecords {
+		exon.WriteString(r.ExtractSequence(scaffoldSeq))
 	}
 
-	prot, err := codonTable.Transrate(Cds)
+	return exon.String()
+}
 
-	if err != nil {
-		fmt.Printf("%v can be invalid CDS, length=%d, length mod 3=%d\n", r.GeneId, len(cds.String()), len(cds.String())%3)
+func (r TranscriptRecords) GenomicSequence(scaffoldSeq string) string {
+	sequence := scaffoldSeq[r.Start:r.End]
+
+	if r.Strand == "-" {
+		sequence = seq.ReverseComplement(sequence)
 	}
 
-	return TranscriptSeq{Genome: genome, Cds: Cds, Mrna: Mrna, Protein: prot}
+	return sequence
 }
 
 func ReadGffFile(gtfPath string) (map[string]TranscriptRecords, error) {
@@ -347,21 +263,18 @@ func ReadGffFile(gtfPath string) (map[string]TranscriptRecords, error) {
 
 	for _, rec := range parser.Records {
 		if rec.IsGeneChild() {
-			// check gene id
-			geneId, found := rec.Attributes.Get("Parent")
-
-			if !found {
-				return nil, fmt.Errorf("")
-			}
-
 			id, found := rec.Attributes.Get("ID")
 
 			if !found {
 				return nil, fmt.Errorf("")
 			}
 
-			recs := TranscriptRecords{GeneId: geneId, Type: rec.Type, SeqName: rec.Seqname, Strand: rec.Strand, Records: []gffio.GffRecord{rec}}
+			var recs TranscriptRecords
+			if recs, err = NewTranscriptRecords(rec); err != nil {
+				return nil, fmt.Errorf("")
+			}
 			id2rec[id] = recs
+
 		} else if rec.IsExon() {
 			// exon can have multiple parents
 			ids, found := rec.Attributes.GetAll("Parent")
@@ -371,7 +284,7 @@ func ReadGffFile(gtfPath string) (map[string]TranscriptRecords, error) {
 
 			for _, id := range ids {
 				recs := id2rec[id]
-				recs.Records = append(recs.Records, rec)
+				recs.PushGffRecord(rec)
 
 				id2rec[id] = recs
 			}
@@ -383,7 +296,7 @@ func ReadGffFile(gtfPath string) (map[string]TranscriptRecords, error) {
 			}
 
 			recs := id2rec[id]
-			recs.Records = append(recs.Records, rec)
+			recs.PushGffRecord(rec)
 
 			id2rec[id] = recs
 		}
